4d2224d2aaba00de987361809a5f9710046d1658
Sean Leas <phoenix_24leas.com>
2020-05-08 22:36:48 -0600

    Complete library function, working through addBook function

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,27 @@
+function createLibrary(name, book) {
+  var library = {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: []
+    }
+  }
+  return library
+}
+
+function addBook(library, book) {
+  var whatGenre = book.genre;
+  console.log('first log', library.shelves.fantasy)
+  if(whatGenre === library.shelves.fantasy) {
+    console.log('second log', whatGenre)
+    library.shelves.fantasy.push(book)
+    }
+}
 
 
 module.exports = {
-  // createLibrary: createLibrary,
-  // addBook: addBook,
+  createLibrary: createLibrary,
+  addBook: addBook,
   // checkoutBook: checkoutBook
 };
