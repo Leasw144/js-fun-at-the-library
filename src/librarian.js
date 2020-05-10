c0b56359d8810a40c9f74b9ab6c3ddcf3c1d1fc1
Sean Leas <phoenix_24leas.com>
2020-05-10 17:55:13 -0600

    Create class for Librarian and methods findBook and greetPatron

--- src/librarian.js
+++ src/librarian.js
@@ -1,8 +1,20 @@
 class Librarian {
-  constructor() {
+  constructor(name, library) {
+    this.name = name;
+    this.library = library
 
   }
-  
+  greetPatron(patron, time) {
+    if(time) {
+      return `Good morning, ${patron}!`   
+    }
+    return `Hello, ${patron}!`
+  }
+
+  findBook(book) {
+
+    return `Yes, we have ${book}`
+  }
 }
 
 module.exports = Librarian;
